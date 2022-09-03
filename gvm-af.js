b0b9785483dc49f470ac790c2965a50f539c053e
TajGVM <112208802+TajGVM@users.noreply.github.com>
2022-09-03 08:34:07 -0600

    Update gvm-af.js

--- gvm-af.js
+++ gvm-af.js
@@ -1,1 +1,7 @@
 //gvm ajax form
+
+var bsnScrpt = createElement('script');
+bsnScrpt.setAttribute('src','https://js.usebasin.com/v1.min.js');
+document.body.appendChild(bsnScrpt);
+var gvmAF = getElementById('gvm-af');
+gvmAF.setAttribute('data-basin-form','');
